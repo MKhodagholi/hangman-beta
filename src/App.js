1a116504f333e67d43def9712ad858357708278d
MKhodagholi <MKhodaagholi@gmail.com>
2022-02-07 13:31:31 +0330

    "add" Switch for App

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
+import { Switch } from "react-router-dom";
+
 import "./App.css";
 
 import Header from "./components/Header/Header";
+import Register from "./components/Register/Register";
 import Welcome from "./components/Welcome/Welcome";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Welcome />
+      <Switch>
+        <Welcome exact path="/" />
+        <Register path="/register" />
+      </Switch>
     </div>
   );
 }
