fb1793f24bef0a44954ad39cde99bdc0f5d12ade
MKhodagholi <MKhodaagholi@gmail.com>
2022-02-07 11:35:15 +0330

    "add" Welocme Component into App Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 
 import Header from "./components/Header/Header";
+import Welcome from "./components/Welcome/Welcome";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Welcome />
     </div>
   );
 }
