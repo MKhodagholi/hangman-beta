90e12513667c92c5c4cb5e8205c26d83b62c705f
MKhodagholi <MKhodaagholi@gmail.com>
2022-02-09 11:56:04 +0330

    create aniamtionJS for pages

---
+++ src/animation.js
@@ -0,0 +1,5 @@
+export const pageAnimation = {
+  hidden: { opacity: 0, x: -1300 },
+  show: { opacity: 1, x: 0, transition: { duration: 1 } },
+  exit: { opacity: 0, x: 1300, transition: { duration: 1 } },
+};
