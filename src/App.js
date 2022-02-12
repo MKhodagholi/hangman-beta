db0dfca4b69c64088df30868cbfe5c780c56ae7a
MKhodagholi <MKhodaagholi@gmail.com>
2022-02-12 08:00:17 +0330

    add exitBefore option for app

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Switch } from "react-router-dom";
+import { Switch, useLocation } from "react-router-dom";
 
 import "./App.css";
 
@@ -6,16 +6,22 @@ import Header from "./components/Header/Header";
 import Welcome from "./components/Welcome/Welcome";
 import Register from "./components/Register/Register";
 import Login from "./components/Login/Login";
+import Game from "./components/Game/Game";
+import { AnimatePresence } from "framer-motion";
 
 function App() {
+  const location = useLocation();
   return (
     <div className="App">
       <Header />
-      <Switch>
-        <Welcome exact path="/" />
-        <Register path="/register" />
-        <Login path="/login" />
-      </Switch>
+      <AnimatePresence exitBeforeEnter>
+        <Switch location={location} key={location.pathname}>
+          <Welcome exact path="/" />
+          <Register path="/register" />
+          <Login path="/login" />
+          <Game path="/game" />
+        </Switch>
+      </AnimatePresence>
     </div>
   );
 }
