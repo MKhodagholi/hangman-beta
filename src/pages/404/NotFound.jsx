9d2c74aff501996781a306b4b417faaa7ea1ab8c
MKhodagholi <MKhodaagholi@gmail.com>
2022-02-23 21:49:04 +0330

    just show this is notFound page

--- src/pages/404/NotFound.jsx
+++ src/pages/404/NotFound.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
+import classes from "./NotFound.module.css";
+
 const NotFound = () => {
-  return <>Not Found...</>;
+  return (
+    <div className={classes["not-found"]}>
+      <div className={classes.text}>صفحه مورد نظر پیدا نشد</div>
+    </div>
+  );
 };
 
 export default NotFound;
