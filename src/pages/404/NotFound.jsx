097ab3ce302edc072e6d2cecf62c28a1f54ee589
MKhodagholi <MKhodaagholi@gmail.com>
2022-02-24 16:09:50 +0330

    add 404 Page

--- src/pages/404/NotFound.jsx
+++ src/pages/404/NotFound.jsx
@@ -1,12 +1,21 @@
 import React from "react";
 
 import classes from "./NotFound.module.css";
+//Animation
+import { motion } from "framer-motion";
+import { pageAnimation } from "../../animation";
 
 const NotFound = () => {
   return (
-    <div className={classes["not-found"]}>
+    <motion.div
+      variants={pageAnimation}
+      initial="hidden"
+      animate="show"
+      exit="exit"
+      className={classes["not-found"]}
+    >
       <div className={classes.text}>صفحه مورد نظر پیدا نشد</div>
-    </div>
+    </motion.div>
   );
 };
 
