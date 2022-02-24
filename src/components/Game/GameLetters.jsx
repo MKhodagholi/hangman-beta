8be4bf65c358fc559fc6e2d626248bc6fbf8c741
MKhodagholi <MKhodaagholi@gmail.com>
2022-02-24 14:59:09 +0330

    Change name GameWrongWords into GameWrongLetters and add WrongLetters into DOM

--- src/components/Game/GameLetters.jsx
+++ src/components/Game/GameLetters.jsx
@@ -2,8 +2,16 @@ import React from "react";
 
 import classes from "./GameLetters.module.css";
 
-const GameLetters = () => {
-  return <div className={classes.word} id="word"></div>;
+const GameLetters = ({ wordDOM }) => {
+  return (
+    <div className={classes.word} id="word">
+      {wordDOM.map((lett, index) => (
+        <div className={classes.letter} key={index}>
+          {lett}
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default GameLetters;
--- src/components/Game/GameWrongLetters.jsx
+++ src/components/Game/GameWrongLetters.jsx
@@ -2,14 +2,18 @@ import React from "react";
 
 import classes from "./GameWrongLetters.module.css";
 
-const GameWrongLetters = () => {
+const GameWrongLetters = ({ wrongLetters }) => {
   return (
     <div
       className={classes["wrong-letters-container"]}
       id="wrong-letters-container"
     >
       <p>حروف اشتباه</p>
-      <div className={classes["wrong-letters"]} id="wrong-letters"></div>
+      <div className={classes["wrong-letters"]} id="wrong-letters">
+        {wrongLetters.map((letter) => (
+          <div className={classes["wrong-letters"]}>{letter}</div>
+        ))}
+      </div>
     </div>
   );
 };
