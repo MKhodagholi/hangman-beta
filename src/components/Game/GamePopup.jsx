7c5252b393a3d93d23e8730a4ef356b1f3115369
MKhodagholi <MKhodaagholi@gmail.com>
2022-02-25 12:12:19 +0330

    use props and fix children

--- src/components/Game/GamePopup.jsx
+++ src/components/Game/GamePopup.jsx
@@ -2,12 +2,17 @@ import React from "react";
 
 import classes from "./GamePopup.module.css";
 
-const GamePopup = () => {
+const GamePopup = ({ popupInfo, nextStepHandler }) => {
+  const { show: isShow, message, word, btnMessage } = popupInfo;
   return (
-    <div className={classes["popup-container"]} id="popup-container">
+    <div
+      className={`${classes["popup-container"]} ${isShow ? classes.show : ""}`}
+      id="popup-container"
+    >
       <div className={classes.popup}>
-        <h2 id="final-message">You Win!</h2>
-        <button id="play-again">بازی مجدد</button>
+        <h2>{message}</h2>
+        <h3>کلمه درست: {word}</h3>
+        <button onClick={nextStepHandler}>{btnMessage}</button>
       </div>
     </div>
   );
